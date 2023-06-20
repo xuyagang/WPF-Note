f11851850e37ab217e38e78747b97de2ceef64ae
Adam <2453760768@qq.com>
2023-06-20 22:21:07 +0800

    资源字典、静态资源和动态资源

---
+++ WPF_资源基础/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_资源基础
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 找到资源,并设置新值
+            this.Resources["SolidColor"] = new SolidColorBrush(Colors.Purple);
+        }
+    }
+}
