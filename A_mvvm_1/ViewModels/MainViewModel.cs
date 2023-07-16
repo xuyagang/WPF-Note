0b80655a20ea4530eb45250a70a2d96cc254069a
Adam <2453760768@qq.com>
2023-07-16 23:21:28 +0800

    mvvmtest

---
+++ A_mvvm_1/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace A_mvvm_1.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            // 把命令通过属性绑定到按钮
+            SearchCommand = new RelayCommand((arg) =>
+            {
+                MessageBox.Show("您点击了按钮");
+            });
+        }
+
+        private string _title = "默认值";
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public ICommand SearchCommand { get; set; }
+
+    }
+}
---
+++ A_mvvm_1/ViewModels/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace A_mvvm_1.ViewModels
+{
+    // 我们不需要每个命令都去创建一个类 
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _executeAction;
+        public RelayCommand(Action<object> action)
+        {
+            _executeAction = action;
+        }
+
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeAction?.Invoke(parameter);
+        }
+    }
+}
