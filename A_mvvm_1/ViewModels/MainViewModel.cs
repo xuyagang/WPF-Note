1d7ca48624b30989a660e07a65ef7a73c70e5175
Adam <2453760768@qq.com>
2023-07-17 08:02:26 +0800

    update

--- A_mvvm_1/ViewModels/MainViewModel.cs
+++ A_mvvm_1/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,25 +9,42 @@ using System.Windows.Input;
 
 namespace A_mvvm_1.ViewModels
 {
-    public class MainViewModel
+    //为了实现属性更新继承通知接口
+    public class MainViewModel : INotifyPropertyChanged
     {
         public MainViewModel()
         {
             // 把命令通过属性绑定到按钮
+            // 把lambda表达式作为委托类型传递给RelayCommand
             SearchCommand = new RelayCommand((arg) =>
             {
                 MessageBox.Show("您点击了按钮");
+
+                Title = "AAA";
             });
+
+
         }
 
         private string _title = "默认值";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string Title
         {
             get { return _title; }
-            set { _title = value; }
+            set 
+            { 
+                _title = value; 
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Title"));
+            }
         }
 
-        public ICommand SearchCommand { get; set; }
+        public ICommand SearchCommand 
+        { 
+            get; 
+            set; 
+        }
 
     }
 }
--- A_mvvm_1/ViewModels/RelayCommand.cs
+++ A_mvvm_1/ViewModels/RelayCommand.cs
@@ -10,6 +10,7 @@ namespace A_mvvm_1.ViewModels
     // 我们不需要每个命令都去创建一个类 
     public class RelayCommand : ICommand
     {
+        // 声明委托对象
         private Action<object> _executeAction;
         public RelayCommand(Action<object> action)
         {
@@ -19,11 +20,18 @@ namespace A_mvvm_1.ViewModels
 
         public event EventHandler CanExecuteChanged;
 
+        public Func<bool> IsCanExecute { get; set; }
+
         public bool CanExecute(object parameter)
         {
-            return true;
+            if (IsCanExecute == null)
+            {
+                return true;
+            }
+            return IsCanExecute.Invoke();
         }
 
+        // 调用命令时会调用该函数
         public void Execute(object parameter)
         {
             _executeAction?.Invoke(parameter);
