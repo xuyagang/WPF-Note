3337026fb4449ea4a9dd3d75fa1cd36ffa662d47
Adam <aaadam@mail.com>
2023-11-03 18:04:11 +0800

    checkbox TreeView

---
+++ A_TreeView/CheckboxTreeView.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A_TreeView
+{
+    /// <summary>
+    /// CheckboxTreeView.xaml 的交互逻辑
+    /// </summary>
+    public partial class CheckboxTreeView : UserControl
+    {
+        List<TreeItem> treeItems;
+        public CheckboxTreeView()
+        {
+            InitializeComponent();
+            InitTree();
+            folderTree.ItemsSource = treeItems;
+        }
+        /// <summary>
+        /// 初始化树
+        /// </summary>
+        void InitTree()
+        {
+            treeItems = new List<TreeItem>();
+            TreeItem t1 = new TreeItem("生产院1");
+
+            TreeItem t1p1 = new TreeItem("项目1");
+            t1p1.Parent = t1;
+            t1.Children.Add(t1p1);
+
+            TreeItem t1p1s1 = new TreeItem("阶段1");
+            t1p1s1.Parent = t1p1;
+            t1p1.Children.Add(t1p1s1);
+
+
+            TreeItem t1p2 = new TreeItem("项目2");
+            t1p2.Parent = t1;
+            t1.Children.Add(t1p2);
+
+
+
+            TreeItem t2 = new TreeItem("生产院2");
+            TreeItem t3 = new TreeItem("生产院3");
+
+
+
+
+            treeItems.Add(t1);
+            treeItems.Add(t2);
+            treeItems.Add(t3);
+        }
+
+        public TreeViewItem CreatTreeItem(TreeView treeView, List<string> treeBranch, string viewName = null)
+        {
+            TreeViewItem mtree = null;
+            ItemCollection itemCollection = treeView.Items;
+            for (int i = 0; i < treeBranch.Count; i++)
+            {
+                bool ifhave = false;
+                for (int j = 0; j < itemCollection.Count; j++)
+                {
+                    mtree = itemCollection[j] as TreeViewItem;
+                    if (mtree.Header.ToString() == treeBranch[i])
+                    {
+                        itemCollection = mtree.Items;
+                        ifhave = true;
+                        break;
+                    }
+                }
+                if (ifhave == false)
+                {
+                    mtree = new TreeViewItem();
+                    mtree.Header = treeBranch[i];
+                    itemCollection.Add(mtree);
+                    itemCollection = mtree.Items;
+                }
+            }
+
+            if (viewName == null) { return mtree; }
+            else
+            {
+                TreeViewItem newtree = new TreeViewItem();
+                newtree.Header = viewName;
+                itemCollection.Add(newtree);
+                return newtree;
+            }
+        }
+
+
+    }
+}
---
+++ A_TreeView/TreeItem.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_TreeView
+{
+    class TreeItem : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 节点文字
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// 父节点
+        /// </summary>
+        public TreeItem Parent { get; set; }
+
+        /// <summary>
+        /// 子节点
+        /// </summary>
+        public ObservableCollection<TreeItem> Children { get; set; }
+        //ObservableCollection 在需要跟踪集合变化并自动在 UI 中反映这些变化的场景中非常有用。
+        //它实现了 INotifyCollectionChanged 接口，该接口在集合中添加、移除或修改项时提供通知
+
+        /// <summary>
+        /// 节点选择状态
+        /// </summary>
+        bool? _isChecked = false;
+        /// <summary>
+        /// 节点选择状态
+        /// </summary>
+        public bool? IsChecked
+        {
+            get => _isChecked;
+            set => SetCheckState(value);
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="name">节点名称</param>
+        public TreeItem(string name)
+        {
+            Text = name;
+            Children = new ObservableCollection<TreeItem>();
+        }
+
+        /// <summary>
+        /// 设置节点状态并同步更新关联父子节点的状态
+        /// </summary>
+        /// <param name="value">当前节点勾选状态</param>
+        /// <param name="updateChildren">是否更新子节点</param>
+        /// <param name="updateParent">是否更新父节点</param>
+        void SetCheckState(bool? value, bool updateChildren = true, bool updateParent = true)
+        {
+            //属性设置时先检查是否和当前值一致
+            if (value == _isChecked) return;
+            _isChecked = value;
+
+            // 对于子节点,如果有值且需要,则递归更新
+            if (updateChildren && _isChecked.HasValue)
+                Children.ToList().ForEach(item => item.SetCheckState(_isChecked,true,false));
+
+            // 对于父节点,如果父节点不为空且有需要,则递归更新
+            if (updateParent && Parent != null)
+                Parent.VerifyCheckState();
+
+            OnPropertyChanged("IsChecked");
+        }
+
+        /// <summary>
+        /// 更新父节点时,递归更新所有父节点
+        /// </summary>
+        void VerifyCheckState()
+        {
+            bool? parentState = null;
+            //遍历子节点计算父节点状态
+            for (int i = 0; i < Children.Count; i++)
+            {
+                bool? current = Children[i].IsChecked;
+                if (i == 0) parentState = current;
+                else if (parentState != current)
+                {
+                    parentState = null;
+                    break;
+                }
+            }
+            // 递归更新父节点
+            SetCheckState(parentState,false);
+        }
+
+        /// <summary>
+        /// 属性值变化事件
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        //当属性值发生变化时，PropertyChangedEventHandler 会被调用，以通知订阅者（通常是 UI 元素）该属性已被更新。
+        //它接受两个参数：发送方对象（通常是拥有该属性的类的实例）和一个 PropertyChangedEventArgs 类的实例，其中包含有关已更改属性的信息
+
+        /// <summary>
+        /// 更新属性
+        /// </summary>
+        /// <param name="propName"></param>
+        void OnPropertyChanged(string propName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
